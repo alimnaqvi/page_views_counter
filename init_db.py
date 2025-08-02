fb3852c21db8ea63f49818d42c596f78138424ee
Ali Mohsin Naqvi <alimohsinn@hotmail.com>
2025-08-02 22:59:38 +0200

    move deployment from Vercel to Render for better Python support

--- init_db.py
+++ init_db.py
@@ -2,9 +2,18 @@
 from dotenv import load_dotenv
 import os
 import psycopg2
+import sys
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "drop":
+        DROP_TABLE = True
+    else:
+        print(f"Unknown argument: {sys.argv[1]}", "The only allowed argument is 'drop'", file=sys.stderr, sep='\n')
+        exit(1)
+else:
+    DROP_TABLE = False
 
 load_dotenv()
-
 try:
     # DB_PATH = os.environ["DB_PATH"]
     CONN_STRING = os.environ["DATABASE_URL"]
@@ -18,6 +27,11 @@ try:
 
         # Open a cursor to perform database operations
         with conn.cursor() as cursor:
+            # Drop the table if it already exists and 'drop' argument is provided
+            if DROP_TABLE:
+                cursor.execute("DROP TABLE IF EXISTS page_views;")
+                print("Dropped table page_views (if it existed).")
+
             # Create a table to store view data
             # Using TEXT for timestamp to store in ISO 8601 format.
             cursor.execute('''
@@ -26,7 +40,7 @@ try:
                     timestamp TEXT NOT NULL,
                     user_agent TEXT,
                     ip_address TEXT
-                )
+                );
             ''')
             print("Finished creating table (if it didn't exist).")
 
